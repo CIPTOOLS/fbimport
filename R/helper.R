9e7aa3dd8f90afed88dc1c4c6694f0a7b4127a1f
omarbenites <omar_cs_7@hotmail.com>
2016-02-29 18:13:58 -0500

    #bug in updating table . See Jenny Bryan example from googlesheets

--- R/helper.R
+++ R/helper.R
@@ -1,32 +1,82 @@
 #fp <- "C:\\Users\\Omar\\Desktop\\SPYL200804_OLJORO.xlsx"
 
+#TEST UNIT 
+
+test(get_fb_sheets(sheets[1])=="Minimal")
+
+library(magrittr)
+library(dplyr)
 
 
-
-fb_sheets <- readxl::excel_sheets(fp)
-
-f <- fb_sheets
-sheet_list <- lapply(X=f, function(x) openxlsx::read.xlsx(xlsxFile = fp,sheet = x, na.strings = TRUE ))
-names(sheet_list) <- fb_sheets
-
-myobj <- structure(sheet_list)
-
-minimal_fbsheet <-  myobj$Minimal
-
-#Test: fb_Minimal_sheet is empty :: TROW AN ERROR
-#library(dplyr)
-#minimal_fbsheet[minimal_fbsheet$Factor=="Short name or Title","Value"]
-minimal_fbname <- as.character(filter(minimal_fbsheet,Factor== "Short name or Title")%>% select(.,Value))[[1]]
-minimal_crop <- as.character(filter(minimal_fbsheet,Factor== "Crop")%>% select(.,Value))[[1]]
-minimal_trial <- as.character(filter(minimal_fbsheet,Factor== "Type of Trial")%>% select(.,Value))[[1]]
-minimal_country <- as.character(filter(minimal_fbsheet,Factor== "Country")%>% select(.,Value))[[1]]
-minimal_begindate <- as.character(filter(minimal_fbsheet,Factor== "Begin date")%>% select(.,Value))[[1]]
-minimal_collaborators <- as.character(filter(minimal_fbsheet,Factor== "Collaborators")%>% select(.,Value))[[1]]
-minimal_leader <- as.character(filter(minimal_fbsheet,Factor== "Leader")%>% select(.,Value))[[1]]
+get_excel_sheet_data <- function(file_path){
+  fb_sheets <- readxl::excel_sheets(file_path)
+  sheet_list <- lapply(X=fb_sheets, function(x) openxlsx::read.xlsx(xlsxFile = file_path,sheet = x, na.strings = TRUE ))
+  names(sheet_list) <- fb_sheets
+  sheet_list <- structure(sheet_list)
+}
 
 
-minimal_file_name <- paste(minimal_fbname,".rda",sep="")
-#in specific repository called import_fieldbook
-save(myobj,minimal_file_name)
+get_minimal_sheet_params <- function(excel_data){
+  
+  minimal_fbsheet <-  excel_data$Minimal
+  #Selection of minimal parameters
+  minimal_fbname <- as.character(dplyr::filter(minimal_fbsheet,Factor== "Short name or Title")%>% dplyr::select(.,Value))[[1]]
+  minimal_crop <- as.character(dplyr::filter(minimal_fbsheet,Factor== "Crop")%>% select(.,Value))[[1]]
+  minimal_trial <- as.character(dplyr::filter(minimal_fbsheet,Factor== "Type of Trial")%>% dplyr::select(.,Value))[[1]]
+  minimal_country <- as.character(dplyr::filter(minimal_fbsheet,Factor== "Country")%>% dplyr::select(.,Value))[[1]]
+  minimal_begindate <- as.character(dplyr::filter(minimal_fbsheet,Factor== "Begin date")%>% dplyr::select(.,Value))[[1]]
+  minimal_collaborators <- as.character(dplyr::filter(minimal_fbsheet,Factor== "Collaborators")%>% dplyr::select(.,Value))[[1]]
+  minimal_leader <- as.character(dplyr::filter(minimal_fbsheet,Factor== "Leader")%>% dplyr::select(.,Value))[[1]]
+  
+  #assembly all parameter in output list
+  output <- list(minimal_fbsheet=minimal_fbsheet, 
+                 minimal_fbname= minimal_fbname,
+                 minimal_crop = minimal_crop,
+                 minimal_trial = minimal_trial,
+                 minimal_country = minimal_country,
+                 minimal_begindate = minimal_begindate,
+                 minimal_collaborators = minimal_collaborators,
+                 minimal_leader = minimal_leader
+                 )
+}
+  
+
+excel_to_rda <- function(excel_data){
+  
+  #rda_name <- excel_data$minimal_data$minimal_fbname
+  rda_name <- get_minimal_sheet_params(excel_data)$minimal_fbname
+  rda_file_name <- paste(rda_name,".rda",sep="")
+  #in specific repository called import_fieldbook
+  save(excel_data,file=rda_file_name)
+  
+}
+
+excel_data <- get_excel_sheet_data(fp)
+minimal_param <- get_minimal_sheet_params(excel_data = excel_data) 
+excel_to_rda(excel_data)
+ 
 
 
+# minimal_fbsheet <-  myobj$Minimal
+# #Test: fb_Minimal_sheet is empty :: TROW AN ERROR
+# #library(dplyr)
+# #minimal_fbsheet[minimal_fbsheet$Factor=="Short name or Title","Value"]
+# minimal_fbname <- as.character(filter(minimal_fbsheet,Factor== "Short name or Title")%>% select(.,Value))[[1]]
+# minimal_crop <- as.character(filter(minimal_fbsheet,Factor== "Crop")%>% select(.,Value))[[1]]
+# minimal_trial <- as.character(filter(minimal_fbsheet,Factor== "Type of Trial")%>% select(.,Value))[[1]]
+# minimal_country <- as.character(filter(minimal_fbsheet,Factor== "Country")%>% select(.,Value))[[1]]
+# minimal_begindate <- as.character(filter(minimal_fbsheet,Factor== "Begin date")%>% select(.,Value))[[1]]
+# minimal_collaborators <- as.character(filter(minimal_fbsheet,Factor== "Collaborators")%>% select(.,Value))[[1]]
+# minimal_leader <- as.character(filter(minimal_fbsheet,Factor== "Leader")%>% select(.,Value))[[1]]
+
+
+# minimal_file_name <- paste(minimal_fbname,".rda",sep="")
+# #in specific repository called import_fieldbook
+# save(myobj,minimal_file_name)
+
+import_fb_data <- data.frame(Fbname=minimal_param$minimal_fbname,
+           Crop=minimal_param$minimal_crop,
+           Trial = minimal_param$minimal_trial, 
+           Country= minimal_param$minimal_country,  Begin_date=minimal_param$minimal_begindate , 
+           Collaborators=minimal_param$minimal_collaborators, 
+           Leader=minimal_param$minimal_leader) 
--- inst/ui.R
+++ inst/ui.R
@@ -9,6 +9,8 @@ library(shinyFiles)
 library(doBy)
 library(DT)
 library(sqldf)
+library(shinysky)
+library(shinyBS)
 
 shinyUI( 
   shinydashboard::dashboardPage(
@@ -43,19 +45,33 @@ shinyUI(
                   tabPanel("Import Data", #begin tabset "CHECK"
                            fluidRow(
                              
-                             shinyFilesButton('file', 'File select', 'Please select a file',FALSE),
+                             #shinyFilesButton('file', 'File select', 'Please select a file',FALSE),
+                             actionButton("tabBut", "Import Fieldbook"),
+                             
+                             bsModal("modalExample", "File Selection", "tabBut", size = "large",
+                                     h2("Import your Excel files"),
+                                     shinyFilesButton('file', 'File select', 'Please select a file',FALSE),
+                                     #DT::dataTableOutput("the_data") ,width = NULL, height = NULL)
+                                     actionButton("id_success","Save",styleclass="success",icon = "ok"),
+                                     shinyalert("shinyalert1", FALSE,auto.close.after = 5)
+                                     ),
+                             br(),
+                             box(
                              #actionButton("calculate", "Calculate",icon("play-circle-o")),
                              #HTML('<div style="float: right; margin: 0 5px 5px 10px;">'),
                              #actionLink('exportButton', 'Download data'),
                              #HTML('</div>'),
                              #p(),
-                             DT::dataTableOutput("the_data") 
-                             
+                             DT::dataTableOutput("the_data") ,width = NULL, height = NULL)
                            )
-                             
-                   
-                           #tags$style(type='text/css', "#exportButton { width:50%; margin-top: 25px;}"),
-                           #tags$style(HTML('#exportButton {background-color:#30c9ae; color: #ffffff}')),
+                          ),
+                          
+                          tags$style(type='text/css', "#tabBut { width:250px; margin-top: 25px;}"),
+                          tags$style(HTML('#tabBut {background-color:#0099cc; color: #ffffff}')),   
+                          tags$style(type='text/css', "#file { width:150px; margin-top: 25px;}"),
+                          tags$style(HTML('#file {background-color:#0099cc; color: #ffffff}'))  
+                          #tags$style(type='text/css', "#exportButton { width:50%; margin-top: 25px;}"),
+                          #tags$style(HTML('#exportButton {background-color:#30c9ae; color: #ffffff}')),
                            
                   )#end tab Panel "CHECK"
                   
@@ -76,5 +92,5 @@ shinyUI(
     )
     
   )
-)
+
 
